eea167cf5313a0db9ea7691d2226c779cd89aa7b
Aayus10 <aayushs.262@gmail.com>
2024-06-12 18:26:05 +0545

    Update project with new features/bug fixes

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,26 +1,26 @@
 import React from "react";
 import "./Portfolio.css";
-import image1 from "./project1.jpg";
+import image1 from "./project1.png";
 import image2 from "./project2.jpg";
 import image3 from "./project3.jpg";
 
 export default function Portfolio() {
   const showProject1 = () => {
-    const websiteUrl = "https://aayus10.github.io/TextUtilityApp/";
+    const websiteUrl = "https://aayus10.github.io/LandingPage/";
 
     // Redirect to the specified website
     window.open(websiteUrl, "_blank");
   };
 
   const showProject2 = () => {
-    const websiteUrl = "https://aayus10.github.io/ReduxToDoList/";
+    const websiteUrl = "https://aayus10.github.io/ToDoListProject/";
 
     // Redirect to the specified website
     window.open(websiteUrl, "_blank");
   };
 
   const showProject3 = () => {
-    const websiteUrl = "https://aayus10.github.io/WeatherApp/";
+    const websiteUrl = "https://aayus10.github.io/MantineUtilityApp/";
 
     // Redirect to the specified website
     window.open(websiteUrl, "_blank");
@@ -34,13 +34,28 @@ export default function Portfolio() {
           <div class="card">
             <img src={image1} class="card-img-top" alt="..." />
             <div class="card-body">
-              <h5 class="card-title">Text Utility App</h5>
+              <h5 class="card-title">Responsive Landing Page</h5>
               <p class="card-text">
-                Designed a simple text utility app in React that can perform
-                simple text manipulations.
+                Landing Page of single page of a website designed using Tailwind
+                and Mantine library.
               </p>
               <div className="view-button">
                 <button onClick={showProject1} type="submit">
+                  VIEW DEMO
+                </button>
+              </div>
+            </div>
+          </div>
+          <div class="card">
+            <img src={image3} class="card-img-top" alt="..." />
+            <div class="card-body">
+              <h5 class="card-title">Utility Application</h5>
+              <p class="card-text">
+                Basic Utility Apps (Text Editor + Weather Application) based on
+                core concepts of React JS
+              </p>
+              <div className="view-button">
+                <button onClick={showProject3} type="submit">
                   VIEW DEMO
                 </button>
               </div>
@@ -56,21 +71,6 @@ export default function Portfolio() {
               </p>
               <div className="view-button">
                 <button onClick={showProject2} type="submit">
-                  VIEW DEMO
-                </button>
-              </div>
-            </div>
-          </div>
-          <div class="card">
-            <img src={image3} class="card-img-top" alt="..." />
-            <div class="card-body">
-              <h5 class="card-title">Weather App</h5>
-              <p class="card-text">
-                Developed a React-based weather application, emphasizing API
-                integration for real-time fetching and display of weather data.
-              </p>
-              <div className="view-button">
-                <button onClick={showProject3} type="submit">
                   VIEW DEMO
                 </button>
               </div>
